d6a3b2e9f38bbf8bb1ef5fd07c3681cf528157f0
Groza Danut <Danut@192.168.11.195>
2024-03-13 15:07:15 +0200

    Enable java util file logging

--- ro.linic.ui.base/src/ro/linic/ui/base/E4LifeCycle.java
+++ ro.linic.ui.base/src/ro/linic/ui/base/E4LifeCycle.java
@@ -1,10 +1,17 @@
 package ro.linic.ui.base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.LogManager;
+
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
+import org.osgi.framework.FrameworkUtil;
 
 import ro.linic.ui.p2.ui.Policy;
 
@@ -13,9 +20,20 @@ public class E4LifeCycle {
 	@PostContextCreate
 	void postContextCreate(final IEclipseContext ctx, @Preference final IEclipsePreferences prefs, 
 			final UISynchronize sync) {
+		registerLogHandler();
 		registerP2Policy(ctx);
 	}
 	
+	private void registerLogHandler() {
+		try {
+			final URL baseUrl = FrameworkUtil.getBundle(getClass()).getEntry("logging.properties");
+			final URL localURL = FileLocator.toFileURL(baseUrl);
+            LogManager.getLogManager().readConfiguration(new FileInputStream(localURL.getFile()));
+        } catch (IOException | SecurityException ex) {
+        	ex.printStackTrace();
+        }
+	}
+
 	private void registerP2Policy(final IEclipseContext ctx) {
 		ctx.set(Policy.class, new CloudPolicy(ctx));
 	}
