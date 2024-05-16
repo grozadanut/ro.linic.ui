d9e0fdd3b8141f1ae46cd52acfaa253fa2b36ce6
Groza Danut <Danut@192.168.11.192>
2024-05-16 16:02:25 +0300

    Update opcoach preferences cache to jakarta

--- ro.linic.ui.e4.help/src/ro/linic/ui/e4/help/handlers/DisplayHelpHandler.java
+++ ro.linic.ui.e4.help/src/ro/linic/ui/e4/help/handlers/DisplayHelpHandler.java
@@ -1,4 +1,4 @@
- 
+
 package ro.linic.ui.e4.help.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
@@ -9,9 +9,9 @@ public class DisplayHelpHandler {
 //	public void execute(final Shell shell, final UserGuide userGuide) {
 //		userGuide.allTutorialContent().ifPresent(html -> new BrowserDialog(shell, Messages.UserGuide, html).open());
 //	}
-	
+
 	@Execute
-    public void execute() throws Exception {
+	public void execute() throws Exception {
 		BaseHelpSystem.getHelpDisplay().displayHelp(false);
-    }
+	}
 }
