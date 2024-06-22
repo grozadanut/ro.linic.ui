ab963f0845c903f8cca4b733861e60ee64610299
Groza Danut <Danut@192.168.11.192>
2024-06-22 07:50:55 +0300

    Work In Progress

--- ro.linic.ui.pos/src/ro/linic/ui/pos/ui/DefaultReceiptUIComponent.java
+++ ro.linic.ui.pos/src/ro/linic/ui/pos/ui/DefaultReceiptUIComponent.java
@@ -1,5 +1,7 @@
 package ro.linic.ui.pos.ui;
 
+import static ro.linic.util.commons.PresentationUtils.EMPTY_STRING;
+
 import java.util.List;
 
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
@@ -9,7 +11,6 @@ import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -24,6 +25,7 @@ import ro.linic.ui.pos.Messages;
 import ro.linic.ui.pos.base.model.PaymentType;
 import ro.linic.ui.pos.base.model.Product;
 import ro.linic.ui.pos.base.model.Receipt;
+import ro.linic.ui.pos.base.model.ReceiptLine;
 import ro.linic.ui.pos.base.model.ReceiptUIComponent;
 import ro.linic.ui.pos.base.services.PosResources;
 import ro.linic.ui.pos.base.services.ProductDataHolder;
@@ -36,12 +38,19 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 	private static final String VERTICAL_SASH_STATE_PREFIX = "receipt.vertical_sash"; //$NON-NLS-1$
 	private static final String HORIZONTAL_SASH_STATE_PREFIX = "receipt.horizontal_sash"; //$NON-NLS-1$
 	
-	private static final Column skuColumn = new Column(0, Product.SKU_FIELD, Messages.SKU, 70);
-	private static final Column nameColumn = new Column(1, Product.NAME_FIELD, Messages.Name, 350);
-	private static final Column uomColumn = new Column(2, Product.UOM_FIELD, Messages.UOM, 70);
-	private static final Column priceColumn = new Column(3, Product.PRICE_FIELD, Messages.Price, 90);
+	private static final Column p_skuColumn = new Column(0, Product.SKU_FIELD, Messages.SKU, 70);
+	private static final Column p_nameColumn = new Column(1, Product.NAME_FIELD, Messages.Name, 350);
+	private static final Column p_uomColumn = new Column(2, Product.UOM_FIELD, Messages.UOM, 70);
+	private static final Column p_priceColumn = new Column(3, Product.PRICE_FIELD, Messages.Price, 90);
+	private static final List<Column> allProductsColumns = List.of(p_skuColumn, p_nameColumn, p_uomColumn, p_priceColumn);
 	
-	private static final List<Column> allProductsColumns = List.of(skuColumn, nameColumn, uomColumn, priceColumn);
+	private static final Column rl_nameColumn = new Column(0, ReceiptLine.NAME_FIELD, Messages.Name, 350);
+	private static final Column rl_priceColumn = new Column(1, ReceiptLine.PRICE_FIELD, Messages.Price, 90);
+	private static final Column rl_uomColumn = new Column(2, ReceiptLine.UOM_FIELD, Messages.UOM, 70);
+	private static final Column rl_quantityColumn = new Column(3, ReceiptLine.QUANTITY_FIELD, Messages.Quantity, 90);
+	private static final Column rl_totalColumn = new Column(4, EMPTY_STRING, Messages.Total, 70);
+	private static final List<Column> allReceiptLineColumns = List.of(rl_nameColumn, rl_priceColumn, rl_uomColumn, rl_quantityColumn,
+			rl_totalColumn);
 	
 	// services
 	private ProductDataHolder productDataHolder;
@@ -53,7 +62,6 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 	private SashForm verticalSash;
 	private SashForm horizontalSash;
 	
-	private Combo searchMode;
 	private Text searchText;
 	private Text quantityText;
 	
@@ -71,7 +79,7 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 	private Button priceOfferButton;
 	
 	private FullFeaturedNatTable<Product> productsTable;
-//	private TreeOperationsNatTable receiptTable;
+	private FullFeaturedNatTable<ReceiptLine> receiptTable;
 	
 	private Label totalNoTaxLabel;
 	private Label taxLabel;
@@ -100,10 +108,10 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(verticalSash);
 		createTopArea(verticalSash);
 		
-//		receiptTable = new TreeOperationsNatTable(TreeOperationsNatTable.SourceLoc.VANZARI);
-//		receiptTable.postConstruct(verticalSash);
-//		receiptTable.getTable().setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
-//		GridDataFactory.fillDefaults().grab(true, true).applyTo(receiptTable.getTable());
+		receiptTable = TableBuilder.with(ReceiptLine.class, allReceiptLineColumns)
+				.build(verticalSash);
+		receiptTable.natTable().setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(receiptTable.natTable());
 
 		createTotalsBar(parent);
 		createBottomBar(parent);
@@ -120,18 +128,12 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 		leftContainer.setLayout(new GridLayout(3, false));
 		GridDataFactory.fillDefaults().applyTo(leftContainer);
 		
-		searchMode = new Combo(leftContainer, SWT.DROP_DOWN);
-//		searchMode.setItems(AllProductsNatTable.ALL_SEARCH_MODES.toArray(new String[] {}));
-		searchMode.select(0);
-		UIUtils.setFont(searchMode);
-		GridDataFactory.fillDefaults().applyTo(searchMode);
-
 		searchText = new Text(leftContainer, SWT.BORDER);
 		searchText.setMessage(Messages.Search);
 		searchText.setTextLimit(255);
 		searchText.setFocus();
 		UIUtils.setBannerFont(searchText);
-		GridDataFactory.fillDefaults().grab(true, false).applyTo(searchText);
+		GridDataFactory.fillDefaults().grab(true, false).span(2, 1).applyTo(searchText);
 
 		quantityText = new Text(leftContainer, SWT.BORDER);
 		quantityText.setMessage(Messages.Quantity);
@@ -169,7 +171,7 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 		closeOtherButton.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 		UIUtils.setFont(closeOtherButton);
 		GridDataFactory.fillDefaults()
-			.grab(true, true)
+			.grab(false, true)
 			.align(SWT.RIGHT, SWT.FILL)
 			.hint(CLOSE_BUTTON_WIDTH, SWT.DEFAULT)
 			.applyTo(closeOtherButton);
@@ -362,7 +364,7 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 	@Override
 	public void persistState() {
 		UIUtils.saveState(PRODUCTS_TABLE_STATE_PREFIX, productsTable.natTable(), part);
-//		UIUtils.saveState(RECEIPT_TABLE_STATE_PREFIX, receiptTable.natTable(), part);
+		UIUtils.saveState(RECEIPT_TABLE_STATE_PREFIX, receiptTable.natTable(), part);
 		
 		final int[] verticalWeights = verticalSash.getWeights();
 		part.getPersistedState().put(VERTICAL_SASH_STATE_PREFIX+".0", String.valueOf(verticalWeights[0])); //$NON-NLS-1$
@@ -373,7 +375,7 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 	}
 	
 	private void loadState() {
-//		UIUtils.loadState(RECEIPT_TABLE_STATE_PREFIX, receiptTable.natTable(), part);
+		UIUtils.loadState(RECEIPT_TABLE_STATE_PREFIX, receiptTable.natTable(), part);
 		UIUtils.loadState(PRODUCTS_TABLE_STATE_PREFIX, productsTable.natTable(), part);
 		
 		final int[] verticalWeights = new int[2];
@@ -401,4 +403,228 @@ public class DefaultReceiptUIComponent implements ReceiptUIComponent {
 		// TODO Auto-generated method stub
 
 	}
+	
+//	private void addListeners() {
+//		search.addModifyListener(e -> allProductsTable.filter(search.getText()));
+//		search.addKeyListener(new KeyAdapter()
+//		{
+//			@Override public void keyPressed(final KeyEvent e)
+//			{
+//				if ((e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR))
+//					if (selectedProduct().isPresent())
+//						addNewOperationToBon(selectedProduct());
+//					else
+//						cantitateText.setFocus();
+//				
+//				if (e.keyCode == SWT.ARROW_DOWN)
+//				{
+//					e.doit = false;
+//					allProductsTable.moveSelection(MoveDirectionEnum.DOWN);
+//				}
+//				if (e.keyCode == SWT.ARROW_UP)
+//				{
+//					e.doit = false;
+//					allProductsTable.moveSelection(MoveDirectionEnum.UP);
+//				}
+//			}
+//		});
+//		
+//		quantityText.addKeyListener(new KeyAdapter()
+//		{
+//			@Override public void keyPressed(final KeyEvent e)
+//			{
+//				if ((e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR) && isNumeric(cantitateText.getText()))
+//					addNewOperationToBon(selectedProduct());
+//
+//				if (e.keyCode == SWT.ARROW_DOWN)
+//				{
+//					e.doit = false;
+//					allProductsTable.moveSelection(MoveDirectionEnum.DOWN);
+//				}
+//				if (e.keyCode == SWT.ARROW_UP)
+//				{
+//					e.doit = false;
+//					allProductsTable.moveSelection(MoveDirectionEnum.UP);
+//				}
+//			}
+//		});
+//		
+//		minusOne.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				quantityText.setText(parse(quantityText.getText()).subtract(BigDecimal.ONE).toString());
+//				search.setFocus();
+//			}
+//		});
+//		
+//		plusOne.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				cantitateText.setText(parse(cantitateText.getText()).add(BigDecimal.ONE).toString());
+//				search.setFocus();
+//			}
+//		});
+//		
+//		plusFive.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				cantitateText.setText(parse(cantitateText.getText()).add(new BigDecimal("5")).toString()); //$NON-NLS-1$
+//				search.setFocus();
+//			}
+//		});
+//		
+//		deleteCant.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				cantitateText.setText("1"); //$NON-NLS-1$
+//				search.setFocus();
+//			}
+//		});
+//		
+//		enter.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				if (isNumeric(cantitateText.getText()))
+//					addNewOperationToBon(selectedProduct());
+//				changeClient();
+//			}
+//		});
+//		
+//		uiCategoriesTable.addSelectionListener(new ISelectionChangedListener()
+//		{
+//			@Override public void selectionChanged(final SelectionChangedEvent event)
+//			{
+//				uiCategoriesTable.selection().stream()
+//				.findFirst()
+//				.ifPresent(uiCat -> allProductsTable.loadData(uiCat.getProducts().stream()
+//						.sorted(Comparator.comparing(Product::getName))
+//						.collect(toImmutableList())));
+//			}
+//		});
+//		
+//		cardOrPhone.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetDefaultSelected(final SelectionEvent e)
+//			{
+//				if (bonCasa == null)
+//				{
+//					MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.VanzareBarPart_MissingDoc,
+//							Messages.VanzareBarPart_MissingDocMessage);
+//					return;
+//				}
+//				
+//				if (ClientSession.instance().isOfflineMode())
+//				{
+//					MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.VanzareBarPart_NotAcc,
+//							Messages.VanzareBarPart_NotAccMessage);
+//					return;
+//				}
+//				
+//				changeClient();
+//			}
+//		});
+//
+//		inchideCasaButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				closeBon(TipInchidere.PRIN_CASA);
+//			}
+//		});
+//		
+//		inchideFacturaBCButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				closeBon(TipInchidere.FACTURA_BC);
+//			}
+//		});
+//		
+//		inchideCardButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				closeBon(TipInchidere.PRIN_CARD);
+//			}
+//		});
+//
+//		cantitateText.addFocusListener(new FocusAdapter()
+//		{
+//			@Override public void focusGained(final FocusEvent e)
+//			{
+//				cantitateText.selectAll();
+//			}
+//		});
+//		
+//		search.addFocusListener(new FocusAdapter()
+//		{
+//			@Override public void focusGained(final FocusEvent e)
+//			{
+//				search.selectAll();
+//			}
+//		});
+//		
+//		managerCasaButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				new ManagerCasaDialog(Display.getCurrent().getActiveShell(), log, ctx).open();
+//			}
+//		});
+//		
+//		incarcaBonuriButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				if (ClientSession.instance().isOfflineMode())
+//				{
+//					MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.VanzareBarPart_Offline, Messages.VanzareBarPart_OfflineMessage);
+//					return;
+//				}
+//				
+//				final AccountingDocument currentBon = BusinessDelegate.reloadDoc(bonCasa);
+//				new VanzariIncarcaDocDialog(Display.getCurrent().getActiveShell(), VanzareBarPart.this, ctx).open();
+//				updateBonCasa(currentBon, true);
+//			}
+//		});
+//
+//		stergeRandButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				deleteOperations(bonDeschisTable.selection().stream()
+//						.collect(toImmutableList()));
+//			}
+//		});
+//
+//		cancelBonButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				if (bonCasa != null && MessageDialog.openQuestion(Display.getCurrent().getActiveShell(), Messages.VanzareBarPart_CancelDoc, Messages.VanzareBarPart_CancelDocMessage))
+//					deleteOperations(ImmutableList.copyOf(bonCasa.getOperatiuni()));
+//			}
+//		});
+//
+//		refreshButton.addSelectionListener(new SelectionAdapter()
+//		{
+//			@Override public void widgetSelected(final SelectionEvent e)
+//			{
+//				if (ClientSession.instance().isOfflineMode())
+//				{
+//					MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.VanzareBarPart_Offline, Messages.VanzareBarPart_OfflineRefreshMessage);
+//					return;
+//				}
+//				
+//				refreshPartners();
+//				reloadProduct(null, true);
+//				updateBonCasa(BusinessDelegate.reloadDoc(bonCasa), true);
+//			}
+//		});
+//	}
 }
