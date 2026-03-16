c11181c66f8939c4160f07157b1a634ea510a3d6
Groza Danut <groza_danut@yahoo.com>
2026-03-16 11:36:49 +0200

    Cache beta tester value

--- ro.linic.ui.legacy/src/ro/linic/ui/legacy/expressions/BetaTester.java
+++ ro.linic.ui.legacy/src/ro/linic/ui/legacy/expressions/BetaTester.java
@@ -1,5 +1,7 @@
- 
 package ro.linic.ui.legacy.expressions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.e4.core.di.annotations.Evaluate;
@@ -12,13 +14,20 @@ public class BetaTester {
 	private static final ILog log = ILog.of(BetaTester.class);
 	public static final String BETA_TESTER_PREF_KEY = "BETA_TESTER"; //$NON-NLS-1$
 	
+	private static Map<String, Boolean> CACHE = new HashMap<>();
+	
 	@Evaluate
 	public boolean evaluate(final AuthenticationSession authSession) {
-		return RestCaller.get("/rest/s1/mantle/my/preference")
-				.internal(authSession.authentication())
-				.addUrlParam("preferenceKey", BETA_TESTER_PREF_KEY)
-				.sync(GenericValue.class, t -> log.error(t.getMessage(), t))
-				.map(gv -> gv.getBoolean("preferenceValue"))
-				.orElse(false);
+		Boolean betaValue = CACHE.get(authSession.authentication().getName());
+		if (betaValue == null) {
+			betaValue = RestCaller.get("/rest/s1/mantle/my/preference")
+					.internal(authSession.authentication())
+					.addUrlParam("preferenceKey", BETA_TESTER_PREF_KEY)
+					.sync(GenericValue.class, t -> log.error(t.getMessage(), t))
+					.map(gv -> gv.getBoolean("preferenceValue"))
+					.orElse(false);
+			CACHE.put(authSession.authentication().getName(), betaValue);
+		}
+		return betaValue;
 	}
 }
